5ea4014eef8d0443b7b7f272f1f4b74099dde582
西村和眞（NISHIMURAKazuma <NISHIMURAKazuma@192.168.0.86>
2026-05-01 12:29:55 +0900

    2026/5/1 q02_advanced.question03まで終了

--- java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question02/SystemMain02.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question02/SystemMain02.java
@@ -1,10 +1,43 @@
 package q02_advanced.question02;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SystemMain02 {
 
 	public static void main(String[] args) {
 
 		//TODO ここから実装する
+		ConsoleReader consoleReader = new ConsoleReader();
+
+		Member miura = Member.getInstance(1, "Passw0rd", "Miura Manabu", 28, 2);
+		Member sato = Member.getInstance(2, "aaaAAA", "Sato Kensuke", 43, 1);
+		List<Member> members = new ArrayList<Member>();
+		members.add(miura);
+		members.add(sato);
+
+		System.out.println("===会員情報を表示します===");
+		MemberManager.showAllMembers(members);
+		System.out.println("===パスワードを変更します===");
+
+		try {
+			System.out.println("input target id>>");
+			int targetId = consoleReader.inputNumber();
+			System.out.println("input new password>>");
+			String newPassword = consoleReader.inputString();
+			MemberManager.updatePassword(members, targetId, newPassword);
+
+		} catch (IOException | NumberFormatException e) {
+			System.out.println("不正な入力です");
+			e.printStackTrace();
+			return;
+		}
+
+		finally {
+			System.out.println("---SHOW DATA---");
+			MemberManager.showAllMembers(members);
+		}
 
 	}
 
--- java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question03/SystemMain03.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question03/SystemMain03.java
@@ -1,14 +1,46 @@
 package q02_advanced.question03;
 
+import java.io.IOException;
+
 public class SystemMain03 {
 
 	public static void main(String[] args) {
+		ConsoleReader cr = new ConsoleReader();
+		MemberStorage ms = new MemberStorage();
 
-		LoginService loginService = new LoginService(new MemberStorage());
-		System.out.println("ログイン情報を入力してください");
-		boolean isLogin = false;
-		//TODO ここから実装する
+		LoginService loginService = new LoginService(ms);
+		Member loginname = null;
 
+		while (loginname == null) {
+
+			try {
+				System.out.println("ログイン情報を入力してください");
+				//				boolean isLogin = false;
+				//TODO ここから実装する
+
+				System.out.println("input id>>");
+				int id = cr.inputNumber();
+				System.out.println("input password>>");
+				String password = cr.inputString();
+				//				MemberManager.updatePassword(members, targetId, newPassword);
+
+				loginname = loginService.doLogin(id, password);
+				if (loginname == null) {
+					System.out.println("ID またはパスワードが違っています。再度入力してください。");
+
+				} else {
+					System.out.println("ログインに成功しました");
+					break;
+				}
+
+			} catch (IOException | NumberFormatException e) {
+				System.out.println("不正な入力です");
+				e.printStackTrace();
+				return;
+			}
+		}
+		System.out.println("ログインユーザ情報を表示します。");
+		loginname.showMember();
 	}
 
 }
